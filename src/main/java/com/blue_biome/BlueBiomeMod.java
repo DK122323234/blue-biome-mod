69497d440ec5f9a9d7e517bbc48ff00812618838
DK122323234 <dkamensky@licei10.ru>
2026-01-26 23:44:52 +0300

    испраавление регестрации

--- src/main/java/com/blue_biome/BlueBiomeMod.java
+++ src/main/java/com/blue_biome/BlueBiomeMod.java
@@ -1,9 +1,12 @@
 package com.blue_biome;
 
+import com.blue_biome.biome.RegisterBiome;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Random;
 
 public class BlueBiomeMod implements ModInitializer {
    public static final String MODID = "blue_biome_mod";
@@ -14,7 +17,7 @@ public class BlueBiomeMod implements ModInitializer {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
-
+        RegisterBiome.register();
         LOGGER.info("Hello Fabric world!");
     }
 }
